0e91e42687b6ab635682bf22389c2d0d93abe649
Jared Sanson <jared@jared.geek.nz>
2014-02-16 17:39:15 +1300

    Implemented basic diagnostic GUI

--- ZeitgeberGUI/MainForm.Designer.cs
+++ ZeitgeberGUI/MainForm.Designer.cs
@@ -28,69 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("CPU", System.Windows.Forms.HorizontalAlignment.Left);
-            System.Windows.Forms.ListViewGroup listViewGroup2 = new System.Windows.Forms.ListViewGroup("Hardware", System.Windows.Forms.HorizontalAlignment.Left);
-            System.Windows.Forms.ListViewGroup listViewGroup3 = new System.Windows.Forms.ListViewGroup("Scheduler", System.Windows.Forms.HorizontalAlignment.Left);
-            System.Windows.Forms.ListViewGroup listViewGroup4 = new System.Windows.Forms.ListViewGroup("Misc", System.Windows.Forms.HorizontalAlignment.Left);
-            System.Windows.Forms.ListViewGroup listViewGroup5 = new System.Windows.Forms.ListViewGroup("Display", System.Windows.Forms.HorizontalAlignment.Left);
-            System.Windows.Forms.ListViewGroup listViewGroup6 = new System.Windows.Forms.ListViewGroup("Power", System.Windows.Forms.HorizontalAlignment.Left);
-            System.Windows.Forms.ListViewGroup listViewGroup7 = new System.Windows.Forms.ListViewGroup("RTC", System.Windows.Forms.HorizontalAlignment.Left);
-            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
-            "VDD",
-            "0 mV"}, -1);
-            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
-            "VBAT",
-            "0 mV"}, -1);
-            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem(new string[] {
-            "Systick",
-            "0"}, -1);
-            System.Windows.Forms.ListViewItem listViewItem4 = new System.Windows.Forms.ListViewItem(new string[] {
-            "Utilization",
-            "0 %"}, -1);
-            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem(new string[] {
-            "Display Usage",
-            "0 %"}, -1);
-            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem(new string[] {
-            "Clock Frequency",
-            "0 Hz"}, -1);
-            System.Windows.Forms.ListViewItem listViewItem7 = new System.Windows.Forms.ListViewItem(new string[] {
-            "BQ25010 Status",
-            ""}, -1);
-            System.Windows.Forms.ListViewItem listViewItem8 = new System.Windows.Forms.ListViewItem(new string[] {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ListViewGroup listViewGroup22 = new System.Windows.Forms.ListViewGroup("CPU", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup23 = new System.Windows.Forms.ListViewGroup("Hardware", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup24 = new System.Windows.Forms.ListViewGroup("Scheduler", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup25 = new System.Windows.Forms.ListViewGroup("Misc", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup26 = new System.Windows.Forms.ListViewGroup("Display", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup27 = new System.Windows.Forms.ListViewGroup("Battery", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup28 = new System.Windows.Forms.ListViewGroup("RTC", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewItem listViewItem19 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Level",
+            "-"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem20 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Voltage",
+            "-"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem21 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Status",
+            "-"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem22 = new System.Windows.Forms.ListViewItem(new string[] {
+            "systick",
+            "-"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem23 = new System.Windows.Forms.ListViewItem(new string[] {
             "Time",
             "-"}, -1);
-            System.Windows.Forms.ListViewItem listViewItem9 = new System.Windows.Forms.ListViewItem(new string[] {
+            System.Windows.Forms.ListViewItem listViewItem24 = new System.Windows.Forms.ListViewItem(new string[] {
             "Date",
             "-"}, -1);
-            System.Windows.Forms.ListViewItem listViewItem10 = new System.Windows.Forms.ListViewItem(new string[] {
-            "Framerate",
-            "0 FPS"}, -1);
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblConnected = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.listViewInfo = new System.Windows.Forms.ListView();
             this.columnHeaderProperty = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeaderValue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timerUpdate = new System.Windows.Forms.Timer(this.components);
             this.groupBox1.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
             this.groupBox2.SuspendLayout();
-            this.groupBox3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // lblConnected
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 7);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Not Connected";
+            this.lblConnected.AutoSize = true;
+            this.lblConnected.Location = new System.Drawing.Point(9, 7);
+            this.lblConnected.Name = "lblConnected";
+            this.lblConnected.Size = new System.Drawing.Size(79, 13);
+            this.lblConnected.TabIndex = 5;
+            this.lblConnected.Text = "Not Connected";
             // 
             // button1
             // 
@@ -100,16 +86,20 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Sync Time";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // groupBox1
             // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.flowLayoutPanel1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 317);
+            this.groupBox1.Location = new System.Drawing.Point(263, 28);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(252, 100);
+            this.groupBox1.Size = new System.Drawing.Size(94, 404);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Actions";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
             // flowLayoutPanel1
             // 
@@ -118,100 +108,84 @@
             this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 16);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(246, 81);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(88, 385);
             this.flowLayoutPanel1.TabIndex = 4;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(84, 3);
+            this.button2.Location = new System.Drawing.Point(3, 32);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
             this.button2.Text = "Reset";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // groupBox2
             // 
-            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox2.Controls.Add(this.listView1);
-            this.groupBox2.Location = new System.Drawing.Point(287, 12);
+            this.groupBox2.Controls.Add(this.listViewInfo);
+            this.groupBox2.Location = new System.Drawing.Point(12, 28);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(236, 420);
+            this.groupBox2.Size = new System.Drawing.Size(245, 404);
             this.groupBox2.TabIndex = 7;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Diagnostics";
             // 
-            // listView1
+            // listViewInfo
             // 
-            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.listViewInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.listViewInfo.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeaderProperty,
             this.columnHeaderValue});
-            this.listView1.FullRowSelect = true;
-            listViewGroup1.Header = "CPU";
-            listViewGroup1.Name = "listViewGroupCPU";
-            listViewGroup2.Header = "Hardware";
-            listViewGroup2.Name = "listViewGroupHardware";
-            listViewGroup3.Header = "Scheduler";
-            listViewGroup3.Name = "listViewGroupScheduler";
-            listViewGroup4.Header = "Misc";
-            listViewGroup4.Name = "listViewGroupMisc";
-            listViewGroup5.Header = "Display";
-            listViewGroup5.Name = "listViewGroupDisplay";
-            listViewGroup6.Header = "Power";
-            listViewGroup6.Name = "listViewGroupPower";
-            listViewGroup7.Header = "RTC";
-            listViewGroup7.Name = "listViewGroupRTC";
-            this.listView1.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
-            listViewGroup1,
-            listViewGroup2,
-            listViewGroup3,
-            listViewGroup4,
-            listViewGroup5,
-            listViewGroup6,
-            listViewGroup7});
-            listViewItem1.Group = listViewGroup6;
-            listViewItem1.StateImageIndex = 0;
-            listViewItem1.ToolTipText = "Supply voltage";
-            listViewItem2.Group = listViewGroup6;
-            listViewItem2.StateImageIndex = 0;
-            listViewItem2.ToolTipText = "Battery voltage";
-            listViewItem3.Group = listViewGroup3;
-            listViewItem3.StateImageIndex = 0;
-            listViewItem4.Group = listViewGroup3;
-            listViewItem4.StateImageIndex = 0;
-            listViewItem5.Group = listViewGroup5;
-            listViewItem5.StateImageIndex = 0;
-            listViewItem6.Group = listViewGroup1;
-            listViewItem6.StateImageIndex = 0;
-            listViewItem7.Group = listViewGroup6;
-            listViewItem7.StateImageIndex = 0;
-            listViewItem8.Group = listViewGroup7;
-            listViewItem8.StateImageIndex = 0;
-            listViewItem9.Group = listViewGroup7;
-            listViewItem9.StateImageIndex = 0;
-            listViewItem10.Group = listViewGroup5;
-            listViewItem10.StateImageIndex = 0;
-            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem1,
-            listViewItem2,
-            listViewItem3,
-            listViewItem4,
-            listViewItem5,
-            listViewItem6,
-            listViewItem7,
-            listViewItem8,
-            listViewItem9,
-            listViewItem10});
-            this.listView1.Location = new System.Drawing.Point(9, 19);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(221, 395);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listViewInfo.FullRowSelect = true;
+            listViewGroup22.Header = "CPU";
+            listViewGroup22.Name = "listViewGroupCPU";
+            listViewGroup23.Header = "Hardware";
+            listViewGroup23.Name = "listViewGroupHardware";
+            listViewGroup24.Header = "Scheduler";
+            listViewGroup24.Name = "listViewGroupScheduler";
+            listViewGroup25.Header = "Misc";
+            listViewGroup25.Name = "listViewGroupMisc";
+            listViewGroup26.Header = "Display";
+            listViewGroup26.Name = "listViewGroupDisplay";
+            listViewGroup27.Header = "Battery";
+            listViewGroup27.Name = "listViewGroupBattery";
+            listViewGroup28.Header = "RTC";
+            listViewGroup28.Name = "listViewGroupRTC";
+            this.listViewInfo.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
+            listViewGroup22,
+            listViewGroup23,
+            listViewGroup24,
+            listViewGroup25,
+            listViewGroup26,
+            listViewGroup27,
+            listViewGroup28});
+            listViewItem19.Group = listViewGroup27;
+            listViewItem20.Group = listViewGroup27;
+            listViewItem21.Group = listViewGroup27;
+            listViewItem22.Group = listViewGroup22;
+            listViewItem23.Group = listViewGroup28;
+            listViewItem24.Group = listViewGroup28;
+            this.listViewInfo.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem19,
+            listViewItem20,
+            listViewItem21,
+            listViewItem22,
+            listViewItem23,
+            listViewItem24});
+            this.listViewInfo.Location = new System.Drawing.Point(9, 19);
+            this.listViewInfo.Name = "listViewInfo";
+            this.listViewInfo.Size = new System.Drawing.Size(230, 379);
+            this.listViewInfo.TabIndex = 0;
+            this.listViewInfo.UseCompatibleStateImageBehavior = false;
+            this.listViewInfo.View = System.Windows.Forms.View.Details;
+            this.listViewInfo.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             // 
             // columnHeaderProperty
             // 
@@ -223,43 +197,29 @@
             this.columnHeaderValue.Text = "Value";
             this.columnHeaderValue.Width = 99;
             // 
-            // groupBox3
+            // timerUpdate
             // 
-            this.groupBox3.Controls.Add(this.pictureBox1);
-            this.groupBox3.Location = new System.Drawing.Point(12, 26);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(144, 155);
-            this.groupBox3.TabIndex = 8;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Display";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Black;
-            this.pictureBox1.Location = new System.Drawing.Point(8, 19);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.timerUpdate.Enabled = true;
+            this.timerUpdate.Interval = 250;
+            this.timerUpdate.Tick += new System.EventHandler(this.timerUpdate_Tick);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(535, 444);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(369, 444);
+            this.Controls.Add(this.lblConnected);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox3);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "Zeitgeber GUI";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.groupBox1.ResumeLayout(false);
             this.flowLayoutPanel1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
-            this.groupBox3.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -267,17 +227,16 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblConnected;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listViewInfo;
         private System.Windows.Forms.ColumnHeader columnHeaderProperty;
         private System.Windows.Forms.ColumnHeader columnHeaderValue;
-        private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timerUpdate;
     }
 }
 
--- ZeitgeberGUI/MainForm.cs
+++ ZeitgeberGUI/MainForm.cs
@@ -7,14 +7,114 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ViscTronics.Zeitlib;
 
 namespace ViscTronics.ZeitgeberGUI
 {
     public partial class MainForm : Form
     {
+        public Zeitgeber zeitgeber = new Zeitgeber();
+        public bool isConnected = false;
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //timerUpdate_Tick(null, null);
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timerUpdate_Tick(object sender, EventArgs e)
+        {
+            // Automatic connection/disconnection
+            if (zeitgeber.isConnected)
+            {
+                try
+                {
+                    zeitgeber.Ping();
+                    UpdateDiagnostics();
+                }
+                catch (Exception)
+                {
+                    lblConnected.Text = "Not Connected";
+                    listViewInfo.Enabled = false;
+                    groupBox1.Enabled = false;
+                }
+            }
+            else
+            {
+                try
+                {
+                    zeitgeber.Connect();
+                    
+                    zeitgeber.Ping();
+                    UpdateDiagnostics();
+
+                    lblConnected.Text = "Connected";
+                    listViewInfo.Enabled = true;
+                    groupBox1.Enabled = true;
+                }
+                catch (Exception)
+                {
+                    // Couldn't connect
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            zeitgeber.SetDateTime(DateTime.Now);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            zeitgeber.Reset();
+        }
+
+        private void UpdateDiagnostics()
+        {
+            listViewInfo.BeginUpdate();
+
+            var cpuInfo = zeitgeber.GetCpuInfo();
+            var cpuGroup = listViewInfo.Groups["listViewGroupCPU"];
+            cpuGroup.Items[0].SubItems[1].Text = cpuInfo.systick.ToString();
+
+            var batteryInfo = zeitgeber.GetBatteryInfo();
+            var batteryGroup = listViewInfo.Groups["listViewGroupBattery"];
+            batteryGroup.Items[0].SubItems[1].Text = batteryInfo.Level.ToString() + "%";
+            batteryGroup.Items[1].SubItems[1].Text = batteryInfo.Voltage.ToString() + "mV";
+            batteryGroup.Items[2].SubItems[1].Text = ((Zeitlib.PowerStatus)batteryInfo.PowerStatus).ToString();
+
+            var dt = zeitgeber.GetDateTime();
+            var rtcGroup = listViewInfo.Groups["listViewGroupRTC"];
+            rtcGroup.Items[0].SubItems[1].Text = dt.ToLongTimeString();
+            rtcGroup.Items[1].SubItems[1].Text = dt.ToShortDateString();
+
+            listViewInfo.EndUpdate();
+
+           // itm.SetValue("Test");
         }
     }
 }
