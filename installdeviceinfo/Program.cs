1b16f9176cbba4ffe27979bf0b7013d523ad9bdb
Jared Sanson <jared@jared.geek.nz>
2014-02-25 22:47:15 +1300

    Added new program for installing device information

---
+++ installdeviceinfo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using ViscTronics.Zeitlib;
+
+namespace installdeviceinfo
+{
+    class Program
+    {
+        /// <summary>
+        /// Installs friendly information for any currently known drivers.
+        /// Note: Windows installs a separate driver for each USB port you plug a device into for some bizzare reason,
+        /// and this will only install info for currently known drivers.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+
+            if (!principal.IsInRole(WindowsBuiltInRole.Administrator))
+                throw new ZeitgeberException("Installing device information requires administrator privileges");
+
+            Console.WriteLine("Installing device information for all currently known devices...");
+
+            Zeitgeber zeitgeber = new Zeitgeber();
+            zeitgeber.InstallDriverInfo();
+
+            Console.WriteLine("Done.");
+        }
+    }
+}
